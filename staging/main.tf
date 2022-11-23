5a40a60b71d35635706f6f386829d23863feda39
igeiman13 <101915037+igeiman13@users.noreply.github.com>
2022-11-23 06:33:08 +0000

    Updated wrapper setting

--- staging/main.tf
+++ staging/main.tf
@@ -37,7 +37,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_security_group" "web-sg" {
   name = "${random_pet.sg.id}-sg"
-  description = "Ingress on port 8080"
+  description = "Ingress on port 808000000"
   ingress {
     from_port   = 8080
     to_port     = 8080
